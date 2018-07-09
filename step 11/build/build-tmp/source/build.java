9ea7974315beb09f8c6a56977260e84fa08f126e
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2018-07-10 00:49:20 +0200

    playing w/ opacity

--- step 11/build/build-tmp/source/build.java
+++ step 11/build/build-tmp/source/build.java
@@ -186,15 +186,17 @@ public void audioDataUpdate(){
 // Called in reductionLab
 public void effecRender(int x, int y, int side, int c, int yOffset){
 	int realColor = 0xffA73CCB;
-	noStroke(); fill(realColor);
-	if(brightness(c) < 20)
-		bottomLef(x, y, side, c, yOffset);
-	if(brightness(c) >= 20 && brightness(c) < 80)
-		bottomRight(x, y, side, c, yOffset);
-	if(brightness(c) >= 80 && brightness(c) < 120)
-		topLeft(x, y, side, c, yOffset);
-	if(brightness(c) >= 120)
-		topRight(x, y, side, c, yOffset);
+	float monochrome = 255 - brightness(c);
+	float alpha = map(monochrome, 0, 255, 50, 255);
+	noStroke(); fill(realColor, monochrome);
+	bottomLef(x, y, side, c, yOffset);
+
+	// if(brightness(c) >= 20 && brightness(c) < 80)
+	// 	bottomRight(x, y, side, c, yOffset);
+	// if(brightness(c) >= 80 && brightness(c) < 120)
+	// 	topLeft(x, y, side, c, yOffset);
+	// if(brightness(c) >= 120)
+	// 	topRight(x, y, side, c, yOffset);
 }
 
 // ================================================================
@@ -206,104 +208,13 @@ public void bottomLef(int x, int y, int side, int c, int yOffset){
 	int realY = y + (padding / 2);
 
 	float b = brightness(c);
-	int sideLab = PApplet.parseInt(map(b, 0 , 255, side, 1));
+	int sideLab = PApplet.parseInt(map(b, 0 , 255, side, side / 2));
 
 	int realSide = sideLab - padding;
-	int x1triangle = realX; 
-	int y1triangle = realY + realSide; 
-	int x2triangle = realX; 
-	int y2triangle = realY; 
-	int x3triangle = realX + realSide; 
-	int y3triangle = realY + realSide; 
-
-	triangle(
-		x1triangle,
-		y1triangle + yOffset, 
-		x2triangle, 
-		y2triangle + yOffset, 
-		x3triangle, 
-		y3triangle + yOffset);
+	ellipse(realX, realY + yOffset, realSide, realSide);
 }
 
-public void bottomRight(int x, int y, int side, int c, int yOffset){
-	// shaping 
-	int padding = (side / 16);
-	int realX = x + (padding / 2);
-	int realY = y + (padding / 2);
 
-	float b = brightness(c);
-	int sideLab = PApplet.parseInt(map(b, 0 , 255, side, 1));
-
-	int realSide = sideLab - padding;
-	int x1triangle = realX; 
-	int y1triangle = realY + realSide; 
-	int x2triangle = realX + realSide; 
-	int y2triangle = realY; 
-	int x3triangle = realX + realSide; 
-	int y3triangle = realY + realSide; 
-
-	triangle(
-		x1triangle,
-		y1triangle + yOffset, 
-		x2triangle, 
-		y2triangle + yOffset, 
-		x3triangle, 
-		y3triangle + yOffset);
-}
-
-public void topLeft(int x, int y, int side, int c, int yOffset){
-	// shaping 
-	int padding = (side / 16);
-	int realX = x + (padding / 2);
-	int realY = y + (padding / 2);
-
-	float b = brightness(c);
-	int sideLab = PApplet.parseInt(map(b, 0 , 255, side, 1));
-
-	int realSide = sideLab - padding;
-
-	int x1triangle = realX + realSide; 
-	int y1triangle = realY; 
-	int x2triangle = realX + realSide;
-	int y2triangle = realY + realSide; 
-	int x3triangle = realX; 
-	int y3triangle = realY; 
-
-	triangle(
-		x1triangle,
-		y1triangle + yOffset, 
-		x2triangle, 
-		y2triangle + yOffset, 
-		x3triangle, 
-		y3triangle + yOffset);
-}
-
-public void topRight(int x, int y, int side, int c, int yOffset){
-	// shaping 
-	int padding = (side / 16);
-	int realX = x + (padding / 2);
-	int realY = y + (padding / 2);
-
-	float b = brightness(c);
-	int sideLab = PApplet.parseInt(map(b, 0 , 255, side, 1));
-
-	int realSide = sideLab - padding;
-
-	int x1triangle = realX; 
-	int y1triangle = realY + realSide; 
-	int x2triangle = realX; 
-	int y2triangle = realY; 
-	int x3triangle = realX + realSide; 
-	int y3triangle = realY + realSide; 
-
-	triangle(
-		x1triangle,
-		y1triangle + yOffset, 
-		x2triangle, 
-		y2triangle + yOffset, 
-		x3triangle, 
-		y3triangle + yOffset);
-}
 
 
 // ================================================================
--- step 11/build/imageEffectLab.pde
+++ step 11/build/imageEffectLab.pde
@@ -5,15 +5,17 @@
 // Called in reductionLab
 void effecRender(int x, int y, int side, color c, int yOffset){
 	color realColor = #A73CCB;
-	noStroke(); fill(realColor);
-	if(brightness(c) < 20)
-		bottomLef(x, y, side, c, yOffset);
-	if(brightness(c) >= 20 && brightness(c) < 80)
-		bottomRight(x, y, side, c, yOffset);
-	if(brightness(c) >= 80 && brightness(c) < 120)
-		topLeft(x, y, side, c, yOffset);
-	if(brightness(c) >= 120)
-		topRight(x, y, side, c, yOffset);
+	float monochrome = 255 - brightness(c);
+	float alpha = map(monochrome, 0, 255, 50, 255);
+	noStroke(); fill(realColor, monochrome);
+	bottomLef(x, y, side, c, yOffset);
+
+	// if(brightness(c) >= 20 && brightness(c) < 80)
+	// 	bottomRight(x, y, side, c, yOffset);
+	// if(brightness(c) >= 80 && brightness(c) < 120)
+	// 	topLeft(x, y, side, c, yOffset);
+	// if(brightness(c) >= 120)
+	// 	topRight(x, y, side, c, yOffset);
 }
 
 // ================================================================
@@ -25,101 +27,10 @@ void bottomLef(int x, int y, int side, color c, int yOffset){
 	int realY = y + (padding / 2);
 
 	float b = brightness(c);
-	int sideLab = int(map(b, 0 , 255, side, 1));
+	int sideLab = int(map(b, 0 , 255, side, side / 2));
 
 	int realSide = sideLab - padding;
-	int x1triangle = realX; 
-	int y1triangle = realY + realSide; 
-	int x2triangle = realX; 
-	int y2triangle = realY; 
-	int x3triangle = realX + realSide; 
-	int y3triangle = realY + realSide; 
-
-	triangle(
-		x1triangle,
-		y1triangle + yOffset, 
-		x2triangle, 
-		y2triangle + yOffset, 
-		x3triangle, 
-		y3triangle + yOffset);
+	ellipse(realX, realY + yOffset, realSide, realSide);
 }
 
-void bottomRight(int x, int y, int side, color c, int yOffset){
-	// shaping 
-	int padding = (side / 16);
-	int realX = x + (padding / 2);
-	int realY = y + (padding / 2);
 
-	float b = brightness(c);
-	int sideLab = int(map(b, 0 , 255, side, 1));
-
-	int realSide = sideLab - padding;
-	int x1triangle = realX; 
-	int y1triangle = realY + realSide; 
-	int x2triangle = realX + realSide; 
-	int y2triangle = realY; 
-	int x3triangle = realX + realSide; 
-	int y3triangle = realY + realSide; 
-
-	triangle(
-		x1triangle,
-		y1triangle + yOffset, 
-		x2triangle, 
-		y2triangle + yOffset, 
-		x3triangle, 
-		y3triangle + yOffset);
-}
-
-void topLeft(int x, int y, int side, color c, int yOffset){
-	// shaping 
-	int padding = (side / 16);
-	int realX = x + (padding / 2);
-	int realY = y + (padding / 2);
-
-	float b = brightness(c);
-	int sideLab = int(map(b, 0 , 255, side, 1));
-
-	int realSide = sideLab - padding;
-
-	int x1triangle = realX + realSide; 
-	int y1triangle = realY; 
-	int x2triangle = realX + realSide;
-	int y2triangle = realY + realSide; 
-	int x3triangle = realX; 
-	int y3triangle = realY; 
-
-	triangle(
-		x1triangle,
-		y1triangle + yOffset, 
-		x2triangle, 
-		y2triangle + yOffset, 
-		x3triangle, 
-		y3triangle + yOffset);
-}
-
-void topRight(int x, int y, int side, color c, int yOffset){
-	// shaping 
-	int padding = (side / 16);
-	int realX = x + (padding / 2);
-	int realY = y + (padding / 2);
-
-	float b = brightness(c);
-	int sideLab = int(map(b, 0 , 255, side, 1));
-
-	int realSide = sideLab - padding;
-
-	int x1triangle = realX; 
-	int y1triangle = realY + realSide; 
-	int x2triangle = realX; 
-	int y2triangle = realY; 
-	int x3triangle = realX + realSide; 
-	int y3triangle = realY + realSide; 
-
-	triangle(
-		x1triangle,
-		y1triangle + yOffset, 
-		x2triangle, 
-		y2triangle + yOffset, 
-		x3triangle, 
-		y3triangle + yOffset);
-}
